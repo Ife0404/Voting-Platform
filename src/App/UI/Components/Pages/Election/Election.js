2771f4104837b2b7600a90d2d516badf36beca4a
Afolabi Praise <ifepraise2004@gmail.com>
2024-07-10 15:42:29 +0100

    feature updated

--- src/App/UI/Components/Pages/Election/Election.js
+++ src/App/UI/Components/Pages/Election/Election.js
@@ -5,9 +5,18 @@ import UserNavbar from '../../Navbar/UserNavbar/UserNavbar';
 function Election() {
   return (
     <div className='election'>
-        <div>
-            <UserNavbar />
+      <div>
+        <UserNavbar />
+      </div>
+      <div className='election-container'>
+        <div className='upcoming'>
+          <div className='upcoming-data'>
+            <p>BBSF election:</p>
+            <div className='date'>@{new Date().getFullYear()}</div>
+          </div>
         </div>
+        <div className='other'></div>
+      </div>
     </div>
   )
 }
--- src/App/UI/Components/Pages/Feature/Feature.js
+++ src/App/UI/Components/Pages/Feature/Feature.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./Feature.css";
 import line from "../../../../Images/svg/line2.svg";
 import lock from "../../../../Images/svg/lock.svg";
@@ -7,8 +7,19 @@ import laptop from "../../../../Images/svg/laptop.svg";
 import hands from "../../../../Images/svg/hand.svg";
 import cheap from "../../../../Images/svg/cheap.svg";
 import easy from "../../../../Images/svg/easy.svg";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 
 function Feature() {
+
+    useEffect(() => {
+        AOS.init({
+            duration: 1000, // Customize the duration of the animations
+            once: true,     // Whether animation should happen only once - while scrolling down
+        });
+    }, []);
+
     return (
         <div className='feature'>
             <div className='feature-content'>
@@ -23,21 +34,27 @@ function Feature() {
                 <div data-aos="fade-up" className='feature-side'>
                     <div className='first-feature'>
                         <img src={lock} alt='altText' />
+                        <p className='feature-words'>Secured by 256 bit encryption</p>
                     </div>
                     <div className='second-feature'>
                         <img src={back} alt='altText' />
+                        <p className='feature-words'>Backend by ethereum based technology</p>
                     </div>
-                    <div className='second-feature'>
+                    <div className='third-feature'>
                         <img src={laptop} alt='altText' />
+                        <p className='feature-words'>Verifiable transactions</p>
                     </div>
-                    <div className='first-feature'>
+                    <div className='fourth-feature'>
                         <img src={hands} alt='altText' />
+                        <p className='feature-words'>Easy to use</p>
                     </div>
-                    <div className='first-feature'>
+                    <div className='fifth-feature'>
                         <img src={cheap} alt='altText' />
+                        <p className='feature-words'>Cheaper than ballot voting system</p>
                     </div>
-                    <div className='first-feature'>
+                    <div className='sixth-feature'>
                         <img src={easy} alt='altText' />
+                        <p className='feature-words'>Faster voting process</p>
                     </div>
                 </div>
             </div>
